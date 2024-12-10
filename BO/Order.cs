295f3a8fb346c827c2ccb4e74d2c4b7877bdd471
HDLC <henri.libault-de-la-chevasnerie@fortil.group>
2024-12-10 17:02:08 +0100

    init project + gitignore

---
+++ BO/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+
+        public string CustomerName { get; set; }
+
+        public string Email { get; set; }
+
+        public string ShippingAddress { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public double TotalAmount { get; set; }
+
+        public string OrderStatus { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+        public int WarehouseId { get; internal set; }
+    }
+}
---
+++ Exercice 4 MVC/Mapping.cs
@@ -0,0 +1,29 @@
+﻿using BO;
+using Exercice_5_MVC.Models;
+
+namespace Exercice_5_MVC
+{
+    public class Mapping
+    {
+       public static WarehouseVM  ConvertToWarehouseVM(Warehouse warehouse)
+        {
+            var vm = new WarehouseVM();
+            vm.Id = warehouse.Id;
+            vm.Address = warehouse.Address;
+            vm.Name = warehouse.Name;
+            vm.PostalCode = warehouse.PostalCode;
+            return vm;
+        }
+
+        public static Warehouse ToWarehouse(WarehouseVM wm)
+        {
+            var warehouse = new Warehouse();
+            warehouse.Id = wm.Id;
+            warehouse.Address = wm.Address;
+            warehouse.Name = wm.Name;
+            warehouse.PostalCode = wm.PostalCode;
+
+            return warehouse;
+        }
+    }
+}
